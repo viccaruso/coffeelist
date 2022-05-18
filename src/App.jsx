275744f93915c8319845b8079d3fd220a6ace998
Victor Caruso <vic.caruso@gmail.com>
2022-05-18 14:56:45 -0700

    Add router and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { Switch, Route, Redirect } from 'react-router-dom';
+import CoffeeList from './components/Coffee/CoffeeList';
+import CoffeeDetails from './components/Coffee/CoffeeDetails';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Switch>
+      <Route path="/list">
+        <CoffeeList />
+      </Route>
+      <Route path="/coffee/:id">
+        <CoffeeDetails />
+      </Route>
+      <Route exact path="/">
+        <Redirect to="/list" />
+      </Route>
+    </Switch>
+  );
 }
