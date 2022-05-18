ba5da12c9c4ada5438a8d07410198fd46d5a186a
Victor Caruso <vic.caruso@gmail.com>
2022-05-18 16:21:06 -0700

    Built out auth hook for user context

--- src/services/client.js
+++ src/services/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const client = createClient(
+export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
 );
--- src/services/users.js
+++ src/services/users.js
@@ -1,20 +1,20 @@
-import { client } from './client';
+import { supabase } from './client';
 
 export function getUser() {
-  return client.auth.user();
+  return supabase.auth.user();
 }
 
-export async function signIn(email, password) {
-  const { user, error } = await client.auth.signIn(email, password);
+export async function supabaseSignIn(email, password) {
+  const { user, error } = await supabase.auth.signIn(email, password);
   if (error) throw error;
   return user;
 }
-export async function signUp(email, password) {
-  const { user, error } = await client.auth.signUp(email, password);
+export async function supabaseSignUp(email, password) {
+  const { user, error } = await supabase.auth.signUp(email, password);
   if (error) throw error;
   return user;
 }
 
-export async function signOut() {
-  return await client.auth.signOut();
+export async function supabaseSignOut() {
+  return await supabase.auth.signOut();
 }
