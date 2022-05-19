642a3e4aca9e42841033d59c53c4ee0bee6ec5f4
Victor Caruso <vic.caruso@gmail.com>
2022-05-18 18:03:09 -0700

    Set up Auth form - handlers need debugging

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,25 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import CoffeeList from './components/Coffee/CoffeeList';
 import CoffeeDetails from './components/Coffee/CoffeeDetails';
 import Auth from './views/Auth/Auth';
+import { UserProvider } from './context/UserContext';
 
 export default function App() {
   return (
-    <Switch>
-      <Route path="/list">
-        <CoffeeList />
-      </Route>
-      <Route path="/coffee/:id">
-        <CoffeeDetails />
-      </Route>
-      <Route path="/login">
-        <Auth />
-      </Route>
-      <Route exact path="/">
-        <Redirect to="/list" />
-      </Route>
-    </Switch>
+    <UserProvider>
+      <Switch>
+        <Route path="/list">
+          <CoffeeList />
+        </Route>
+        <Route path="/coffee/:id">
+          <CoffeeDetails />
+        </Route>
+        <Route path="/login">
+          <Auth />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/list" />
+        </Route>
+      </Switch>
+    </UserProvider>
   );
 }
--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,3 +1,40 @@
+import { useState } from 'react';
+import { useAuth } from '../../hooks/auth';
+
 export default function Auth() {
-  return <div>Auth</div>;
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const { signUp, signIn } = useAuth();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await signIn(email, password);
+  };
+
+  const handleClick = async () => {
+    await signUp(email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email" />
+      <input
+        name="email"
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label htmlFor="password" />
+      <input
+        name="password"
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Sign In</button>
+      <button type="button">Sign Up</button>
+    </form>
+  );
 }
