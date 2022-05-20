9375267366475fa27129c0be078e3578026217fd
Victor Caruso <vic.caruso@gmail.com>
2022-05-20 15:50:01 -0700

    Add link to coffee detail page

--- src/components/Coffee/Coffee.jsx
+++ src/components/Coffee/Coffee.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Coffee({ coffee }) {
   const date = new Date(coffee.created_at);
   return (
-    <li>
-      <span>{date.toLocaleDateString()}</span>
+    <>
+      <li>
+        <span>{date.toLocaleDateString()}</span>
 
-      <span>{coffee.roastery}</span>
+        <span>{coffee.roastery}</span>
 
-      <span>{coffee.name}</span>
-    </li>
+        <span>{coffee.name}</span>
+      </li>
+      <Link to={`/coffee/${coffee.id}`}>View Details</Link>
+    </>
   );
 }
